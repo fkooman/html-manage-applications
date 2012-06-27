5a78299a562f4b104240aea25ad6c1a5d3bc8597
François Kooman <fkooman@tuxed.net>
2012-06-27 12:00:48 +0200

    add README, update manage URI

--- manage.js
+++ manage.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var apiRoot = 'http://localhost/phpvoot';
+    var apiRoot = 'http://localhost/php-oauth';
     var apiScopes = ["oauth_admin", "oauth_approval", "oauth_userinfo"];
     var apiClientId = 'manage';
     jso_configure({
